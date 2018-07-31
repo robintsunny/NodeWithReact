980056b60d5869c5dd3d60c5250d46cd8fe269fa
robin <robintsunny@gmail.com>
2018-07-30 20:39:21 -0700

    Google Auth

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({bye: 'bro'})
-})
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    accessToken => {
+        console.log(accessToken)
+    }
+))
 
-// app.get('/3', (req,res) => {
-//     res.send({no: 'nope'})
-// })
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile','email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT) 
