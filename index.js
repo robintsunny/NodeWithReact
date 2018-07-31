136d9af9e0fb6fd476943326345891e3ca38dfe2
robin <robintsunny@gmail.com>
2018-07-31 14:34:07 -0700

    Install Mongoose and set up MLAB

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const mongoose = require('mongoose')
+require('./services/passport')
+const keys = require('./config/keys')
+const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
-passport.use(new GoogleStrategy(
-    {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken);
-        console.log('refreshToken', refreshToken);
-        console.log('profile', profile);
 
-    }
-))
+authRoutes(app)
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile','email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT) 
