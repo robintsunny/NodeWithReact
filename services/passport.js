a4a9f874b0b44764b00ffe3d0d8f71acde93071f
robin <robintsunny@gmail.com>
2018-08-01 20:34:18 -0700

    Log User In

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,15 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null,user)
+    })
+})
 
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -12,8 +21,16 @@ passport.use(new GoogleStrategy({
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        new User ({
-            googleId: profile.id
-        }).save()
+        User.findOne({googleId: profile.id})
+            .then((existingUser) => {
+                if (existingUser) {
+                    done(null, existingUser)
+                } else {
+                    new User ({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user))
+                }
+            })
+
     }
 ))
