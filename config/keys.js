d4f6741726422e5e0625637c48db20c012f27493
robin <robintsunny@gmail.com>
2018-08-06 12:41:54 -0700

    Create Client Side

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // keys.js
 
-if (prove.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
 } else {
     module.exports = require('./dev')
--- services/passport.js
+++ services/passport.js
@@ -20,17 +20,16 @@ passport.use(new GoogleStrategy({
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id})
-            .then((existingUser) => {
+
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id})
                 if (existingUser) {
                     done(null, existingUser)
                 } else {
-                    new User ({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user))
+                    const user = await new User ({ googleId: profile.id }).save()
+                    done(null,user)
                 }
-            })
+            }
+    )
 
-    }
-))
+)
