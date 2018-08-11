765688d8c153f60b22a916f8d92b1aa0aaab3984
robin <robintsunny@gmail.com>
2018-08-10 17:37:29 -0700

    Add Stripe Payment

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import Payments from './Payments'
 
 class Header extends React.Component {
 
@@ -11,7 +12,10 @@ class Header extends React.Component {
             case false:
                 return <li><a href="/auth/google">Login with Google</a></li>
             default:
-                return <li><a href="/api/logout">Logout</a></li>
+                return [
+                    <li key="1" ><Payments /></li>,
+                    <li key = "2" > < a href = "/api/logout" > Logout < /a></li >
+                ]
 
         }
     }
