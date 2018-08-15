0b8a8fcd7752cd99d4a504cf6cd0d817ff7e71ba
robin <robintsunny@gmail.com>
2018-08-15 15:40:19 -0700

    Email Surveys to Recipients

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose')
+
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
+const Survey = mongoose.model('surveys')
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, (req,res) => {
+        const {title, subject, body, recipients} = req.body
 
+        const survey = new Survey({
+            title: title,
+            body: body,
+            subject: body,
+            recipients: recipients.split(',').map(email => ({
+                return {email: email.trim()}
+            })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        })
     })
 }
