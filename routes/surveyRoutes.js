2defea1f10a1e5129a0c5cde0e5d8b69e522951a
robin <robintsunny@gmail.com>
2018-08-16 14:53:54 -0700

    Successfully Send Email

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,13 +7,13 @@ const Mailer = require('../services/mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, (req,res) => {
+    app.post('/api/surveys', requireLogin, async (req,res) => {
         const {title, subject, body, recipients} = req.body
 
         const survey = new Survey({
             title: title,
-            body: body,
-            subject: body,
+            body: body, 
+            subject: subject,
           recipients: recipients.split(',').map(email => ({
               email: email.trim()
           })),
@@ -22,5 +22,17 @@ module.exports = app => {
         })
 
         const mailer = new Mailer(survey, surveyTemplate(survey))
+        try {
+
+            await mailer.send()
+            
+            await survey.save()
+            req.user.credits -= 1;
+            const user = await req.user.save()
+            
+            res.send(user)
+        } catch (err) {
+            res.status(422).send(err)
+        }
     })
 }
